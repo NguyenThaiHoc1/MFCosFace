68b2b4ae114b6c81a80d45cdebccfa9b9a032891
NguyenThaiHoc1 <thaihocmap123@gmail.com>
2022-05-06 14:40:22 +0700

    Edit get infor from tfrecords

--- 03_get_info_dataset.py
+++ 03_get_info_dataset.py
@@ -1,27 +1,58 @@
 import os
 from pathlib import Path
+
 from Settings import config
+from Tensorflow.TFRecord import tfrecord
 
 if __name__ == '__main__':
 
-    dataset = Path('/Volumes/Ventoy') / 'Data' / 'data_fujinet' # config.TRAIN_DATASET_RAW_FOLDER  #  # config.TRAIN_DATASET_RESULT_FOLDER / 'lfw' / 'merge'
-    extentions_img = '*.jpg'
-    dict_count = {
-        'num_classes': 0,
-        'num_images': 0
-    }
-    for folder_name in os.listdir(dataset):
+    path_check = False
 
-        folder_path = dataset / str(folder_name)
+    if path_check:
 
-        len_file = os.listdir(folder_path)
+        dataset = Path(
+            '/Volumes/Ventoy') / 'Data' / 'data_fujinet'  # config.TRAIN_DATASET_RAW_FOLDER  #  # config.TRAIN_DATASET_RESULT_FOLDER / 'lfw' / 'merge'
 
-        if len(len_file) == 0:
-            print(folder_name)
+        extentions_img = '*.jpg'
+        dict_count = {
+            'num_classes': 0,
+            'num_images': 0
+        }
+        for folder_name in os.listdir(dataset):
 
-        for filename in folder_path.glob(extentions_img):
-            dict_count['num_images'] += 1
+            folder_path = dataset / str(folder_name)
 
-        dict_count['num_classes'] += 1
+            len_file = os.listdir(folder_path)
 
-    print(f"Number of Person: {dict_count['num_classes']} - Number of images: {dict_count['num_images']}")
+            if len(len_file) == 0:
+                print(folder_name)
+
+            for filename in folder_path.glob(extentions_img):
+                dict_count['num_images'] += 1
+
+            dict_count['num_classes'] += 1
+
+        print(f"Number of Person: {dict_count['num_classes']} - Number of images: {dict_count['num_images']}")
+
+    else:
+
+        dataset = config.TRAIN_DATASET_RESULT_FOLDER / 'data_cfp_lfw.tfrecords'
+
+        loader_dataset = tfrecord.TFRecordData().load(dataset,
+                                                      is_repeat=False,
+                                                      binary_img=True,
+                                                      is_crop=False,
+                                                      reprocess=False,
+                                                      shuffle=False,
+                                                      batch_size=1)
+        num_classes = set()
+        num_images = 0
+        for inputs, labels in loader_dataset:
+
+            num_classes.add(int(labels.numpy()[0]))
+            num_images += 1
+
+        print(f"Number of Person: {len(num_classes)} - Number of images: {num_images}")
+
+
+
